082e85f6c9ac9962dd1c0b6f27091ffcdfbe737b
AssortRock0 <AssortRock0@ASSORTROCK>
2024-01-04 15:17:06 +0900

    ConsoleScreen 라이브러리 생성 Galaga에서 사용할 수 있게 설정

---
+++ ConsoleEngine/ConsoleScreen.cpp
@@ -0,0 +1,7 @@
+#include "ConsoleScreen.h"
+#include <iostream>
+
+void ConsoleScreen::Test()
+{
+	printf_s("라이브러리 성공");
+}
---
+++ Galaga/Galaga.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include<ConsoleEngine/ConsoleScreen.h>
+
+int main()
+{
+    ConsoleScreen NewScreen = ConsoleScreen();
+
+    NewScreen.Test();
+
+    std::cout << "Hello World!\n";
+}
