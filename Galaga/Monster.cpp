ccbced42efb24e8795e14cc4c3738d5d2c0cd347
AssortRock0 <AssortRock0@ASSORTROCK>
2024-01-15 16:50:41 +0900

    몬스터 내려오기 첫줄만 단체로 내려옴

--- Galaga/Monster.cpp
+++ Galaga/Monster.cpp
@@ -1,10 +1,12 @@
 #include "Monster.h"
-#include "ContentsEnum.h"
 
+#include "ContentsEnum.h"
+#include <ConsoleEngine/EngineCore.h>
 
 void Monster::Update()
 {
 	ConsoleObject* CollisionObject = Collision(GalagaUpdateType::Bullet);
+	EngineCore* Core = GetCore();
 
 	if (nullptr != CollisionObject)
 	{
@@ -12,4 +14,48 @@ void Monster::Update()
 		CollisionObject->Destroy();
 	}
 
+	std::list<ConsoleObject*> MonsterList = Core->GetUpdateGroup(static_cast<int>(GalagaUpdateType::Monster));
+	std::list<ConsoleObject*>::iterator StartIter = MonsterList.begin();
+	std::list<ConsoleObject*>::iterator EndIter = MonsterList.end();
+
+	if (0 == (GetPos().Y % 2))
+	{
+		if (Core->Screen.GetScreenX() == GetPos().X)
+		{
+			
+			for (; StartIter != EndIter; ++StartIter)
+			{
+				ConsoleObject* MonsterPos = *StartIter;
+
+				if (MonsterPos->GetPos().Y == GetPos().Y)
+				{
+					MonsterPos->AddPos(Down);
+				}
+			}
+		}
+		else
+		{
+			AddPos(Right);
+		}
+	}
+
+	if (0 != (GetPos().Y % 2))
+	{
+		if (0 == GetPos().X)
+		{
+			for (; StartIter != EndIter; ++StartIter)
+			{
+				ConsoleObject* MonsterPos = *StartIter;
+
+				if (MonsterPos->GetPos().Y == GetPos().Y)
+				{
+					MonsterPos->AddPos(Down);
+				}
+			}
+		}
+		else
+		{
+			AddPos(Left);
+		}
+	}
 }
